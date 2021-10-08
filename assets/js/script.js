595535e694e19b51adbb54c2c614b469b87c8720
Elves Queiroz <elves.waq@gmail.com>
2021-10-07 23:43:17 -0300

    responsividade do site e menu adicionados

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+function abrirMenu(){
+    var menu = document.getElementById("menu-mobile");
+   if (menu.style.display == 'none' || menu.style.display == ''){
+        menu.style.display = 'block';          
+       }else{
+           menu.style.display = 'none';
+       }
+}
